f19eca64e6080e466421881913f30ea3cd531f1b
Mohamed Salou <msalou@gmx.de>
2021-12-04 23:15:17 +0100

    first commit

---
+++ src/main/java/de/coronacheck/coronacheckbw/CoronaCheckBwApplication.java
@@ -0,0 +1,13 @@
+package de.coronacheck.coronacheckbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaCheckBwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaCheckBwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/coronacheck/coronacheckbw/CoronaCheckBwApplicationTests.java
@@ -0,0 +1,14 @@
+package de.coronacheck.coronacheckbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaCheckBwApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
