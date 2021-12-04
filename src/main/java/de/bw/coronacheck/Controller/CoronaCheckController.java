85b57d6f1f74e08b25875eca28b9eb053c2912f1
Mohamed Salou <msalou@gmx.de>
2021-12-05 00:46:39 +0100

    Add service

--- src/main/java/de/bw/coronacheck/Controller/CoronaCheckController.java
+++ src/main/java/de/bw/coronacheck/Controller/CoronaCheckController.java
@@ -1,14 +1,20 @@
 package de.bw.coronacheck.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.bw.coronacheck.service.CoronaCheckService;
 
 @RestController
 public class CoronaCheckController {
 
+  @Autowired
+  private CoronaCheckService coronaCheckService;
+
   @GetMapping("/")
   public String get() {
-    return "Hello World";
+    return coronaCheckService.getOptions();
   }
 
 }
---
+++ src/main/java/de/bw/coronacheck/service/CoronaCheckService.java
@@ -0,0 +1,12 @@
+package de.bw.coronacheck.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoronaCheckService {
+
+    public String getOptions() {
+        return "Hello World.";
+    }
+    
+}
